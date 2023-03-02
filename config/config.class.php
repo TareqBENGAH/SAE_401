e91abc1b199352f781fb555447d4bc243520ac31
Pagrune <pagrune@orange.fr>
2023-03-02 11:36:57 +0100

    création routeur et config + vues

---
+++ config/config.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class config{
+    public const DBHOST ="localhost";
+    public const DBNAME ="401_sae";
+    public const DBUSER ="root";
+    public const DBPWD="";
+
+    public const MENU="<a class='lien' href='index.php?action=aventures'>Les aventures</a>
+    <a class='lien' href='index.php?action=cadeaux'>Les cadeaux</a>
+    <a class='lien' href='index.php?action=faq'>F.A.Q</a>
+    <a class='lien' href='index.php?action=contact'>Contact</a>"
+}
--- controleur/ctlRouteur.class.php
+++ controleur/ctlRouteur.class.php
@@ -1,9 +1,30 @@
-<php
+<?php
 
 class ctlRouteur{
 
     public function Routage(){
         try{
+            if(isset($_GET["action"])){
+                switch($_GET["action"]){
+                    case "aventures":
+                        require "vue/vue_aventure.php";
+                        break;
+                    case "cadeaux":
+                        require "vue/vue_cadeaux.php";
+                        break;
+                    case "faq":
+                        require "vue/vue_faq.php";
+                        break;
+                    case "contact":
+                        require "vue/vue_contact.php";
+                        break;
+                    default:
+                        require "vue/vue_accueil.php";
+                        break;
+                }
+            }
+            else
+                require "vue/vue_accueil.php";
             
         }
     }
