221384abb3277b6ba72b4f41b219490f08f3dd53
Pagrune <pagrune@orange.fr>
2023-03-09 15:22:52 +0100

    responsive header + début vue aventure

--- config/config.class.php
+++ config/config.class.php
@@ -11,7 +11,9 @@ class config{
     public const MENU="<a class='lien' href='index.php?action=aventures'>Les aventures</a>
     <a class='lien' href='index.php?action=cadeaux'>Les cadeaux</a>
     <a class='lien' href='index.php?action=faq'>F.A.Q</a>
-    <a class='lien' href='index.php?action=contact'>Contact</a>";
+    <a class='lien' href='index.php?action=contact'>Contact</a>
+    <a class='lien' href='index.php?action=panier'><img src='img/header/paniers.png' alt='Icône de panier'></a>
+    <a class='lien' href='index.php?action=connexion'><img src='img/header/utilisateur.png' alt='Icône connexion'></a>";
 
     public const FOOTER = "
     <a class='lien' href='index.php?action=cgv'>CGV</a><p> | </p>
---
+++ vue/vue_aventures.php
@@ -0,0 +1,66 @@
+<div id="aventures">
+    <div class="block-1">
+        <div>
+            <h1>Plongez au coeur de <span>nos aventures</span></h1>
+            <img class="img_boussole" src="img/page_aventures/boussole.jpg" alt="Photo d'une carte avec une boussole">
+        </div>
+        <div class="block-2">
+            <div class="texte-block-1">
+                <p>
+                        Notre entreprise d'Escape Game en extérieur offre une expérience unique pour les amateurs de défis, d'aventure et d'exploration.
+                </p>
+                <p>
+                        Nos jeux ont lieu dans des lieux pittoresques en plein air, tels que des parcs, des forêts, des rues historiques ou des quartiers animés, ce qui permet aux joueurs de découvrir de nouveaux endroits tout en résolvant des énigmes et en travaillant en équipe.
+                </p>
+            </div>
+            <div class="text-block-2">
+                <p>
+                    Préparez-vous à affronter des défis uniques et à vivre une aventure inoubliable en plein air grâce à notre Escape Game en extérieur&nbsp;!
+                </p>
+            </div>
+        </div>
+    </div>
+    <div id="aventure_famille">
+        <h2>Les aventures à faire en famille</h2>
+        <div class="block_trio_aventure">
+            <div class="block_aventure">
+                <h3>Kredo Castello</h3>
+                <img src="img/kredo/kredo.png" alt="Image escape game Kredo">
+                <div class="genre_aventure">
+                    <p>
+                        Genre :
+                    </p>
+                    <p>
+                        <span>Fantastique</span>
+                    </p>
+                </div>
+                <div>
+                    <div>
+                        <img src="img/icons/Clock.png" alt="Icône durée">
+                        <p>Durée : 2 heures | environ 2,5 km</p>
+                    </div>
+                    <div>
+                        <img src="img/icons/GeoAlt.png" alt="Icône localisation">
+                    </div>
+                </div>
+                <p>
+                    <span>
+                        Convient aux joueurs débutants
+                    </span>
+                </p>
+                <div class="button_ensavoir">
+                    <button class="button_plus">
+                            <a>En savoir plus</a>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div id="aventure_debutant">
+        <h2>Les aventures pour les plus novices</h2>
+    </div>
+    <div id="aventure_confirmée">
+        <h2>Les aventures pour les plus confirmés</h2>
+    </div>
+</div>
