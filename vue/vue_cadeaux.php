aa660eb2340ffd9961eab2960d4763486cc86e7c
Pagrune <pagrune@orange.fr>
2023-03-20 22:51:55 +0100

    contenu vue carte cadeau

--- vue/vue_cadeaux.php
+++ vue/vue_cadeaux.php
@@ -1,12 +1,24 @@
 <div id="cadeau">
     <div class="block-1">
-        <img src="" alt="">
+        <img src="img/page_cadeaux/carte_cadeau.jpg" alt="Carte cadeau">
         <div class="block-cadeau">
             <h1>
                 Carte Cadeau
             </h1>
+            <p>
+                Vous ne savez pas quoi offrir pour un anniversaire ou tout autre occasion. Vous cherchez un cadeau original qui surprendra son destinataire ?
+            </p>
+            <p>
+                Ne cherchez-plus, vous êtes au bon endroit !
+            </p>
             
-
+            <form action="">
+                <label for="montant_carte">
+                    Montant de la carte en €
+                    <input type="number" id="montant_carte" name="cadeau" min="20" max="1000" placeholder="€">
+                </label>
+                <input id="achat_carte" name="achat_carte" type="submit" value="Acheter une carte cadeau">
+            </form>
         </div>
     </div>
 </div>
