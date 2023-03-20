28074e1313c9c93bc3b0fa1dd8b04740ad8625d5
Pagrune <pagrune@orange.fr>
2023-03-20 22:06:58 +0100

    vue carte cadeau

---
+++ vue/vue_cadeaux.php
@@ -0,0 +1,12 @@
+<div id="cadeau">
+    <div class="block-1">
+        <img src="" alt="">
+        <div class="block-cadeau">
+            <h1>
+                Carte Cadeau
+            </h1>
+            
+
+        </div>
+    </div>
+</div>
